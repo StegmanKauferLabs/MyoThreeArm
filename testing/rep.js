597ad1abe8d74ee0b0a0ee5aa67621b4b502720b
Matthew Kaufer <mjkaufer@gmail.com>
2015-12-01 13:37:25 -0500

    Badass helper functions to determine closest compressed feature to a point

--- testing/rep.js
+++ testing/rep.js
@@ -22,3 +22,69 @@ function getFeatures(data){
 	var top100 = Math.abs(copy[copy.length - 1] - mean)
 	return {'mean': mean, 'top80': top80, 'top100': top100}
 }
+
+//determine how probable a point is in a data set - lower is better
+function calculateUncertainty(pointValue, featureObject){
+	
+	/*
+
+			|mean - point|
+	0.8  *  --------------
+				top80
+
+			|mean - point|
+	+ 0.2 * --------------
+				top100
+
+
+		Math.abs((mean - point) / (top80 - point)) 
+
+		0.8 * Math.abs(Math.abs(mean - point) - top80) + 0.2 * Math.abs(Math.abs(mean - point) - top100)
+
+	*/
+
+	var mean = featureObject.mean
+	var top80 = featureObject.top80//fyi, I think numbers in variable names are horrible - they were pierce's idea
+	var top100 = featureObject.top100
+
+	return (
+		0.8 * Math.abs(mean - pointValue) / top80 +
+		0.2 * Math.abs(mean - pointValue) / top100
+	)
+
+}
+
+//pointValue is a number, featureObjects is an array of the type of objects which come from the getFeatures method
+//returns a reference to the closest featureObject, along with its percentage of uncertainty, in an object
+/*
+{
+	feature: featureObject,
+	uncertainty: ...
+}
+*/
+
+function findClosestFeatureObject(pointValue, featureObjects){
+
+	var totalProbability = 0
+
+	var bestUncertainty = calculateUncertainty(pointValue, featureObjects[0])
+	var bestIndex = 0
+
+	for(var i = 1; i < featureObjects.length; i++){
+		var currentUncertainty = calculateUncertainty(pointValue, featureObjects[i])
+
+		totalProbability += currentUncertainty * currentUncertainty//so the smaller things get smaller and the bigger things get bigger
+
+		if(currentUncertainty < bestUncertainty){
+			bestUncertainty = currentUncertainty
+			bestIndex = i
+		}
+	}
+
+	return {
+		feature: featureObject,
+		uncertainty: bestUncertainty * bestUncertainty / totalProbability
+	}
+
+
+}
